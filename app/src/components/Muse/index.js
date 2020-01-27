7f42ea9f73bfc3fcdfd7f4a80693718e16dc052c
abrie <abrie@users.noreply.github.com>
2020-01-27 09:14:38 -0500

    Looping with resets.

--- app/src/components/Muse/index.js
+++ app/src/components/Muse/index.js
@@ -1,67 +1,83 @@
-import React from "react";
+import React, { useEffect } from "react";
 import LFSR from "../LFSR";
 import { ShufflePopper } from "../Shuffle";
 import Transport from "../Transport";
 import * as Tone from "tone";
+import seedrandom from "seedrandom";
 
 export default function() {
-  const seed = "100101";
-  const initial = parseInt(seed, 2);
-  const lfsr_a = new LFSR(seed.length, initial);
+  function run() {
+    const seed = "1001011";
+    const initial = parseInt(seed, 2);
+    const lfsr_a = new LFSR(seed.length, initial);
 
-  const seed_b = "100000";
-  const initial_b = parseInt(seed_b, 2);
-  const lfsr_b = new LFSR(seed_b.length, initial_b);
+    const seed_b = "1000001";
+    const initial_b = parseInt(seed_b, 2);
+    const lfsr_b = new LFSR(seed_b.length, initial_b);
 
-  const kick = new Tone.MembraneSynth().toMaster();
-  const snare = new Tone.NoiseSynth().toMaster();
-  const hat = new Tone.MetalSynth().toMaster();
-  hat.volume.value = -20;
-  const synth = new Tone.PolySynth(3, Tone.Synth).toMaster();
+    const reset = () => {
+      seedrandom("hello", { global: true });
+      lfsr_a.register = initial;
+      lfsr_b.register = initial;
 
-  const SONG_LENGTH = "32m";
+      notes = ShufflePopper([...makeMajor(60), ...makeMajor(60 + 7)]);
+      durations = ShufflePopper(makeTimeBase(["16n", "8n", "32n", "32n"]));
+    };
 
-  const actions_a = [
-    time =>
-      kick.triggerAttackRelease("C2", "4n", time, Math.random() * 0.25 + 0.75),
-    time => snare.triggerAttackRelease("2n", time),
-    time => hat.triggerAttackRelease("8n", time, Math.random() * 0.5 + 0.5)
-  ];
+    const kick = new Tone.MembraneSynth().toMaster();
+    const snare = new Tone.NoiseSynth().toMaster();
+    const hat = new Tone.MetalSynth().toMaster();
+    hat.volume.value = -20;
+    const synth = new Tone.PolySynth(3, Tone.Synth).toMaster();
 
-  const percussionLoop = new Tone.Loop(time => {
-    lfsr_a.shift();
-    const idx = lfsr_a.register % 4;
-    const action = actions_a[idx];
-    if (action) action(time);
-  }, "8n");
-  percussionLoop.start("0m").stop(SONG_LENGTH);
+    const SONG_LENGTH = "3m";
 
-  const major = [0, 4, 7, 9];
-  const makeMajor = root =>
-    major.map(step => Tone.Frequency(root + step, "midi"));
-  const notes = ShufflePopper([...makeMajor(60), ...makeMajor(60 + 7)]);
+    Tone.Transport.scheduleRepeat(reset, SONG_LENGTH, "0m");
 
-  const makeTimeBase = arr => arr.map(not => Tone.TimeBase(not));
-  const durations = ShufflePopper(makeTimeBase(["16n", "8n", "32n", "32n"]));
+    const actions_a = [
+      t =>
+        kick.triggerAttackRelease("C2", "4n", t, Math.random() * 0.25 + 0.75),
+      t => snare.triggerAttackRelease("2n", t),
+      t => hat.triggerAttackRelease("8n", t, Math.random() * 0.5 + 0.5)
+    ];
 
-  const actions_b = [
-    (time, dur) => synth.triggerAttackRelease(notes.next().value, dur, time),
-    (time, dur) => synth.triggerAttackRelease(notes.next().value, dur, time)
-  ];
+    const percussionLoop = new Tone.Loop(time => {
+      lfsr_a.shift();
+      const idx = lfsr_a.register % 4;
+      const action = actions_a[idx];
+      if (action) action(time);
+    }, "8n");
+    percussionLoop.start("0m").stop(SONG_LENGTH);
 
-  var remaining = 0;
-  const synthLoop = new Tone.Loop(time => {
-    if (remaining <= 0) {
-      remaining = durations.next().value;
-      lfsr_b.shift();
-      const idx = lfsr_b.register % 4;
-      const action = actions_b[idx];
-      if (action) action(time, remaining);
-    } else {
-      remaining -= Tone.TimeBase("64n");
-    }
-  }, "64n");
-  synthLoop.start("0m").stop(SONG_LENGTH);
+    const major = [0, 4, 7, 9];
+    const makeMajor = root =>
+      major.map(step => Tone.Frequency(root + step, "midi"));
+    var notes = ShufflePopper([...makeMajor(60), ...makeMajor(60 + 7)]);
+
+    const makeTimeBase = arr => arr.map(not => Tone.TimeBase(not));
+    var durations = ShufflePopper(makeTimeBase(["16n", "8n", "32n", "32n"]));
+
+    const actions_b = [
+      (time, dur) => synth.triggerAttackRelease(notes.next().value, dur, time),
+      (time, dur) => synth.triggerAttackRelease(notes.next().value, dur, time)
+    ];
+
+    var remaining = 0;
+    const synthLoop = new Tone.Loop(time => {
+      if (remaining <= 0) {
+        remaining = durations.next().value;
+        lfsr_b.shift();
+        const idx = lfsr_b.register % 4;
+        const action = actions_b[idx];
+        if (action) action(time, remaining);
+      } else {
+        remaining -= Tone.TimeBase("64n");
+      }
+    }, "64n");
+    synthLoop.start("0m").stop(SONG_LENGTH);
+  }
+
+  useEffect(() => run(), []);
 
   return <Transport />;
 }
--- app/src/components/Transport/index.js
+++ app/src/components/Transport/index.js
@@ -1,14 +1,19 @@
 import React, { useState, useEffect } from "react";
 import * as Tone from "tone";
+import "./style.css";
 
 export default function() {
   const [playing, setPlaying] = useState(false);
-  const [position, setPosition] = useState(0);
+  const [position, setPosition] = useState(Tone.Transport.position);
 
   useEffect(() => {
+    Tone.Transport.setLoopPoints(0, "3m");
+    Tone.Transport.loop = true;
     Tone.Transport.scheduleRepeat(
-      () => setPosition(Tone.Transport.position),
-      "16n",
+      () => {
+        setPosition(Tone.Transport.position.split(".")[0]);
+      },
+      "32n",
       "0m"
     );
   }, []);
@@ -25,13 +30,15 @@ export default function() {
 
   return (
     <div>
-      <button onClick={play} disabled={playing}>
-        play
-      </button>
-      <button onClick={stop} disabled={!playing}>
-        stop
-      </button>
-      {position}
+      <div className="positionIndicator">{position}</div>
+      <div>
+        <button onClick={play} disabled={playing}>
+          play
+        </button>
+        <button onClick={stop} disabled={!playing}>
+          stop
+        </button>
+      </div>
     </div>
   );
 }
