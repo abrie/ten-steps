1e4ea2be3a8960b41c0a094745e1d9c8f5bf3c65
abrie <abrie@users.noreply.github.com>
2020-01-26 19:20:55 -0500

    Implement start/stop logic and UI.

--- app/src/components/Prototype/index.js
+++ app/src/components/Prototype/index.js
@@ -3,9 +3,16 @@ import * as Tone from "tone";
 
 export default function() {
   const [synth, setSynth] = useState(null);
+  const [playing, setPlaying] = useState(false);
+  const [tick, setTick] = useState(0);
 
   useEffect(() => {
     setSynth(new Tone.Synth().toMaster());
+    Tone.Transport.scheduleRepeat(
+      () => setTick(Tone.Transport.ticks),
+      "1i",
+      "0m"
+    );
   }, []);
 
   const play = () => {
@@ -14,11 +21,23 @@ export default function() {
     }, "8n");
     loop.start("0m").stop("1m");
     Tone.Transport.start();
+    setPlaying(true);
+  };
+
+  const stop = () => {
+    Tone.Transport.stop();
+    setPlaying(false);
   };
 
   return (
     <div>
-      <button onClick={play}>play</button>
+      <button onClick={play} disabled={playing}>
+        play
+      </button>
+      <button onClick={stop} disabled={!playing}>
+        stop
+      </button>
+      {tick}
     </div>
   );
 }
